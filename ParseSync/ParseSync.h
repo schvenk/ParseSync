af3a528527fc41e179f2dd959c1783a1d0a84c1d
Dave Feldman <dfeldman@interfacethis.com>
2012-03-22 12:43:25 -0700

    Initial Commit

---
+++ ParseSync/ParseSync.h
@@ -0,0 +1,13 @@
+//
+//  ParseSync.h
+//  ParseSync
+//
+//  Created by David Feldman on 3/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ParseSync : NSObject
+
+@end
---
+++ ParseSync/ParseSync.m
@@ -0,0 +1,13 @@
+//
+//  ParseSync.m
+//  ParseSync
+//
+//  Created by David Feldman on 3/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ParseSync.h"
+
+@implementation ParseSync
+
+@end
